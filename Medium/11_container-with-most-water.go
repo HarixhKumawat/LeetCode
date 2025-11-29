7bb0bc7a8511116f3fd52237f8ff6a93332289e5
Harixh <harish.g.kumawat@gmail.com>
2025-11-29 22:56:17 +0530

    345 and 11

--- Medium/11_container-with-most-water.go
+++ Medium/11_container-with-most-water.go
@@ -4,21 +4,30 @@ import "fmt"
 
 func maxArea(height []int) int {
 	max := 0
+	left := 0
+	right := len(height) - 1
 
-	for i := 0; i < len(height); i++ {
-		for j := i + 1; j < len(height); j++ {
-			h := height[i]
-			if height[j] < height[i] {
-				h = height[j]
-			}
-			distance := j - i
-			area := h * distance
+	for left < right {
+		width := right - left
+		h := height[left]
 
-			if area > max {
-				max = area
-			}
+		if height[right] < h {
+			h = height[right]
+		}
+
+		area := width * h
+
+		if area > max {
+			max = area
+		}
+
+		if height[left] < height[right] {
+			left++
+		} else {
+			right--
 		}
 	}
+
 	return max
 }
 
